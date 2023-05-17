fc9aafbcbe09a5a69f21fcaed289aff49538c3cc
0day <70134418+0dayReaper@users.noreply.github.com>
2023-05-17 01:20:56 -0400

    Fixed resource display issues

--- src/app.js
+++ src/app.js
@@ -1,28 +1,32 @@
 import countries from './countries.js';
 
 window.onload = () => {
-    const resourcesContainer = document.getElementById('resources');
+  const resourcesContainer = document.getElementById('resources');
   
-    countries.forEach(country => {
-        const countryObject = country[0];
-        const countryName = Object.keys(countryObject)[0];
-        const countryResources = countryObject[countryName];
+  console.log('countries:', countries);
   
-        const countryElement = document.createElement('div');
-        countryElement.innerHTML = `
-            <h2>${countryName}</h2>
-            <ul>
-                ${countryResources.map(resource => `
-                    <li>
-                        <a href="${resource.url}" target="_blank">${resource.name}</a>
-                        ${resource.url_info ? `<span class="info">${resource.url_info}</span>` : ''}
-                        ${resource.phone.length > 0 ? `<span class="info">- Phone: ${resource.phone.map(phone => `<a href="tel:${phone}">${phone}</a>`).join(' or ')}</span>` : ''}
-                        ${resource.phone_info ? `<span class="info">${resource.phone_info}</span>` : ''}
-                    </li>
-                `).join('')}
-            </ul>
-        `;
-        resourcesContainer.appendChild(countryElement);
-    });
-  };
-  
+  countries.forEach(country => {
+    const countryName = Object.keys(country)[0];
+    const countryResources = country[countryName];
+    
+    console.log('countryName:', countryName);
+    console.log('countryResources:', countryResources);
+    
+    const countryElement = document.createElement('div');
+    countryElement.innerHTML = `
+      <h2>${countryName}</h2>
+      <ul>
+        ${countryResources.map(resource => `
+          <li>
+            <a href="${resource.url}" target="_blank">${resource.name}</a>
+            ${resource.url_info ? `<span class="info">${resource.url_info}</span>` : ''}
+            ${resource.phone.length > 0 ? `<span class="info">- Phone: ${resource.phone.map(phone => `<a href="tel:${phone}">${phone}</a>`).join(' or ')}</span>` : ''}
+            ${resource.phone_info ? `<span class="info">${resource.phone_info}</span>` : ''}
+          </li>
+        `).join('')}
+      </ul>
+    `;
+    
+    resourcesContainer.appendChild(countryElement);
+  });
+};
