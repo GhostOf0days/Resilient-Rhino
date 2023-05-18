301a45573fac01abb48974f7fe20014347eb2c72
0day <70134418+0dayReaper@users.noreply.github.com>
2023-05-18 01:55:46 -0400

    Fixed variable name issue

--- netlify/functions/fetch-analytics.js
+++ netlify/functions/fetch-analytics.js
@@ -2,6 +2,6 @@ exports.handler = async () => {
     const uniqueVisitors = process.env.UNIQUE_VISITORS;
     return {
       statusCode: 200,
-      body: JSON.stringify({ secretValue })
+      body: JSON.stringify({ uniqueVisitors })
     };
   };  
