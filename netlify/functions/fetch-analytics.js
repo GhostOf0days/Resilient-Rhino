3ba738dad19a38129978f7e3ba62018a7f12ecf4
0day <70134418+0dayReaper@users.noreply.github.com>
2023-05-18 01:55:08 -0400

    Fixed unique visitor count

--- netlify/functions/fetch-analytics.js
+++ netlify/functions/fetch-analytics.js
@@ -1,20 +1,7 @@
-const axios = require('axios');
-
-exports.handler = async (event, context) => {
-  try {
-    const response = await axios.get(`https://api.netlify.com/api/v1/sites/${process.env.NETLIFY_SITE_ID}/analytics/data`, {
-      headers: {
-        'Authorization': `Bearer ${process.env.NETLIFY_ACCESS_TOKEN}`
-      }
-    });
-
+exports.handler = async () => {
+    const uniqueVisitors = process.env.UNIQUE_VISITORS;
     return {
       statusCode: 200,
-      body: JSON.stringify({
-        uniqueVisitors: response.data.data.unique_visitors
-      })
+      body: JSON.stringify({ secretValue })
     };
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() };
-  }
-};
+  };  
