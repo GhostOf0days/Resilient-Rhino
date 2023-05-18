0bc928775621a974f3de3d38b77bffa8109449cf
0day <70134418+0dayReaper@users.noreply.github.com>
2023-05-18 13:39:55 -0400

    Added fish easter egg

---
+++ src/fishCursor.js
@@ -0,0 +1,6 @@
+// This is an easter egg revolving around Carnegie Mellon's ScottyLabs
+
+// Function to handle the click event on the "t" in "Resilient Rhino"
+function handleTClick(event) {
+    document.body.classList.add('cursor-fish');
+}
