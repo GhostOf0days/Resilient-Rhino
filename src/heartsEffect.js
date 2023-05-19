4c6a4050f29d96b701a608dff323f9a29a623fbe
0day <70134418+0dayReaper@users.noreply.github.com>
2023-05-19 15:02:21 -0400

    Fixed Hearts Effect

--- src/heartsEffect.js
+++ src/heartsEffect.js
@@ -1,16 +1,17 @@
 document.body.addEventListener('mousemove', function(e) {
     let heart = document.createElement('div');
     heart.classList.add('heart');
-    heart.style.left = (e.pageX - 25) + 'px'; // Position the heart at the cursor's x position
-    heart.style.top = (e.pageY - 25) + 'px'; // Position the heart at the cursor's y position
+    heart.style.left = (e.pageX - 15) + 'px'; // Position the heart at the cursor's x position
+    heart.style.top = (e.pageY - 15) + 'px'; // Position the heart at the cursor's y position
     heart.style.pointerEvents = 'none';
     document.body.appendChild(heart);
 
-    setTimeout(() => { // Make the heart disappear after 0.5 seconds
+    setTimeout(() => { // Start reducing the heart's opacity after a short delay
+        heart.style.transition = 'opacity 0.5s ease-in-out';
         heart.style.opacity = 0;
-    }, 500);
+    }, 100);
 
-    setTimeout(() => { // Remove the heart from the document after 1 second
+    setTimeout(() => { // Remove the heart from the document after it has fully faded
         document.body.removeChild(heart);
-    }, 1000);
+    }, 600);
 });
