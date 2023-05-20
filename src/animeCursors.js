af3529d219d4d083045b547e85d052d760b53034
0day <70134418+0dayReaper@users.noreply.github.com>
2023-05-20 16:16:18 -0400

    Added Spirited Away Cursors

--- src/animeCursors.js
+++ src/animeCursors.js
@@ -1,6 +1,7 @@
 // Anime Cursors dictionary for anime easter egg
 window.animeCursors = {
     'Chibi Kiki Cursor (Kiki\'s Delivery Service)': '../public/cursors/anime/chibiKikiCursor.cur',
+    'Chihiro Ogino Cursor (Spirited Away)': '../public/cursors/anime/chihiroOginoCursor.cur',
     'Chomusuke Cursor (Konosuba)': '../public/cursors/anime/chomusukeCursor.cur',
     'Jiji Cursor (Kiki\'s Delivery Service)': '../public/cursors/anime/jijiCursor.cur',
     'Kaori Violin Cursor (Your Lie in April)': '../public/cursors/anime/kaoriViolinCursor.cur',
@@ -11,6 +12,7 @@ window.animeCursors = {
     'Mai Sakurajima Cursor (Rascal Does Not Dream of Bunny Girl Senpai)': '../public/cursors/anime/maiSakurajimaCursor.cur',
     'Megumin Cursor (Konosuba)': '../public/cursors/anime/meguminCursor.cur',
     'Mitsuha Miyamizu Cursor (Your Name)': '../public/cursors/anime/mitsuhaMiyamizuCursor.cur',
+    'No-Face Cursor (Spirited Away)': '../public/cursors/anime/noFaceCursor.cur',
     'Pochita Cursor (Chainsaw Man)': '../public/cursors/anime/pochitaCursor.cur',
     'Totoro Cursor (My Neighbor Totoro)': '../public/cursors/anime/totoroCursor.cur',
     'Totoro And Friends Cursor (My Neighbor Totoro)': '../public/cursors/anime/totoroAndFriendsCursor.cur',
